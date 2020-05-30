6adaecbaaafd36eea14687a973ec169e102c7feb
Your Name Dorinetk <dketchiozo@gmail.com>
2020-05-30 00:48:04 -0400

    app components testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Dorine Ketchiozo </h1>
+      <h2> About Me</h2>
+      <div>
+        <div> </div>
+        <div>
+         
+          <p>
+            
+            I am a passionate, innovative, experienced and hardworking Web
+            Developer, with extensive experience in website management. I
+            recently graduated from the UNC Charlotte Full Stack Coding
+            Bootcamp. I have acquired coding skills in HTML, CSS, Javascript,
+            JQuery, Node, React, MySQL, NoSQL, MongoDB, Bootstrap, Material-UI,
+            and other web developer skills like APIs, AGILE process... During my
+            career, I diligently managed a USG content management based
+            bilingual website (in English and French). I successfully led
+            several teams during website migration projects, including training
+            non-technical colleagues on the internal policy, content submission
+            and website maintenance. I also have strong experience in
+            incorporation of social media and public affairs programs in website
+            content. Providing excellent customer service experiences is my
+            daily satisfaction. I believe in constant professional growth
+            through self-training and training others. As a web developer, I
+            build professional, mobile responsive, SEO friendly and secured
+            websites and full stack apps for businesses and individuals. My
+            portfolio showcases a few recent projects. Do not hesitate to
+            contact me. It will be my pleasure to bring to live your next web
+            design idea or to join your team. Visit: GitHub Repository |
+            LinkedIn Account
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
