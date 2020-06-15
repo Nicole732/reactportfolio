b2e8003a5b4244cdda160b7d15337a2a860c642c
Your Name Dorinetk <dketchiozo@gmail.com>
2020-06-15 13:52:58 -0400

    edited app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import Home from "../Home";
 import NavBar from "../NavBar";
 import About from "../About";
 import Contact from "../Contact";
-import Footer from "../Footer"
+import Footer from "../Footer";
+import Portfolio from "../Portfolio";
 
-const App = () => {
+function App () {
 
   return (
     <Router>
       <div>
         <NavBar />
-        <Route exact path="/"> component= {Home} </Route>
+        <Route exact path="/"> component= {About} </Route>
         <Route path="/about"> component= {About} </Route>
         <Route path="/portfolio"> component= {Portfolio} </Route>
         <Route path="/contact"> component= {Contact} </Route>
