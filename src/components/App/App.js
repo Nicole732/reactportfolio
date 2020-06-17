fa9a48fd220d0123d4ec475fd138cf07cb6c00eb
Your Name Dorinetk <dketchiozo@gmail.com>
2020-06-17 15:27:59 -0400

    revising state with portfolio component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import NavBar from "../NavBar";
-import About from "../About";
+import About from "../../pages/About/About";
 import Contact from "../Contact";
 import Footer from "../Footer";
-import Portfolio from "../Portfolio";
+import Portfolio from "../../pages/Portfolio/Portfolio";
 
-function App () {
-
-  return (
-    <Router>
+class App extends Component {
+  render() {
+    return (
       <div>
-        <NavBar />
-        <Route exact path="/"> component= {About} </Route>
-        <Route path="/about"> component= {About} </Route>
-        <Route path="/portfolio"> component= {Portfolio} </Route>
-        <Route path="/contact"> component= {Contact} </Route>
-        <Footer />
+        {/*  <Router>
+        //<div>
+        //<NavBar /> 
+          //<Route exact path="/"> component= {About} </Route>
+         // <Route path="/about"> component= {About} </Route>
+          //<Route path="/portfolio"> component= {Portfolio} </Route>
+         //<Route path="/contact"> component= {Contact} </Route>
+         // <Footer /> 
+         // </div>
+         //</Router> */}
+        <About />
+        <Portfolio />
       </div>
-
-    </Router>
-  );
-};
+    );
+  }
+}
 
 export default App;
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Portfolio extends Component {
+  state = {
+    languages: [
+      { name: "Html", proficiency: "Advanced" },
+      { name: "CSS", proficiency: "Advanced" },
+      { name: "Javascript", proficiency: "Advanced" },
+      { name: "React", proficiency: "Advanced" }
+    ]
+  }
+  render() {
+    return (
+        <div>
+            <h1>Languages and Technologies Proficiency</h1>
+            <button> View </button>
+            
+
+        </div>
+    );
+  }
+}
