897b103469833d9cee568cf6bc4db3eb758c8e83
Your Name Dorinetk <dketchiozo@gmail.com>
2020-07-05 15:34:02 -0400

    added 404 not found page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,33 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import NavBar from "../NavBar/index";
+import NavBar from "../NavBar/";
 import Homepage from '../../pages/Homepage';
 import About from "../../pages/About/About";
 import Contact from "../../pages/Contact/Contact";
 import Footer from "../Footer";
 import Portfolio from "../../pages/Portfolio/Portfolio";
+import NotFoundPage from "../../pages/NotFoundPage";
 
 class App extends Component {
   render() {
     return (
       <div>
-         <Router>
-         <div>
-          <NavBar />
-          <Route exact path="/" component={Homepage} />       
-          <Route exact path="/about"  component={About} />  
-          <Route exact path="/portfolio" component={Portfolio} />  
-         <Route exact path="/contact" component={Contact} /> 
-          {/* <Footer /> */}
+        <Router>
+          <div>
+            <NavBar />
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+            <Route path="/" component={NotFoundPage} />
+            {/* <Footer /> */}
           </div>
-          </Router> 
-         {/* <Homepage />
+        </Router>
+        {/* <Homepage />
           <About />
          <Portfolio /> */}
-      
-    </div>
+
+      </div>
     );
   }
 }
